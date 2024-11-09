18c25d5b4440bd123af103c71713150f50064979
Paula Risteiu <paula.risteiu@gmail.com>
2024-11-09 08:46:29 +0200

    LAB 1

--- src/test/java/AngajatiApp/repository/EmployeeMockTestWBT.java
+++ src/test/java/AngajatiApp/repository/EmployeeMockTestWBT.java
@@ -6,12 +6,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-class EmployeeMockTest2 {
+class EmployeeMockTestWBT {
 
    private EmployeeMock employeeMock;
     @BeforeEach
